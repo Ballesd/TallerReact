68aad01cafe007d0b8d6242da7e6ad023b7e0d87
Danny <djvasquezce@unal.edu.co>
2022-05-18 12:33:48 -0500

    danny componente tabla

--- src/Componentes_Principal.jsx
+++ src/Componentes_Principal.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import {Formulario} from './Fomulario';
+import {Tabla} from './Tabla'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export function Aplicacion(){
     return(
         <React.Fragment>
             <Formulario/>
+            <Tabla/>
+            <Tabla/>
+            <Tabla/>
+        
         </React.Fragment>
     )
 }

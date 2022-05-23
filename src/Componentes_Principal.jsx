46f7a506d60838c8c4cffca4bf0081db0a118d76
Erik <erpalacioc@unal.edu.co>
2022-05-22 23:17:55 -0500

    Nabvar ERIK

--- src/Componentes_Principal.jsx
+++ src/Componentes_Principal.jsx
@@ -3,10 +3,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {Formulario} from './Fomulario';
 import {Tabla} from './Tabla'
 import {Boton} from './boton';
+import {Nabvar} from './Nabvar';
 
 export function Aplicacion(){
     return(
         <React.Fragment>
+            <Nabvar/>
             <Tabla/>
             <Formulario/>
             <Tabla/>
